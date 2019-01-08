686175c114ae1c81deb6caaa8fc18002184e9d4b
Wentao <wentao.feng@epfl.ch>
2019-01-08 16:25:58 +0100

    Revert "Merge branch 'master' of https://github.com/zhaodongsun/DronePhotography"
    This reverts commit 286e945ef1d648fec38d7e21eeb67069b318f078, reversing
    changes made to e9c5afd92bb6e6df836673ed31984785dbfc1f4f.

--- mobile/src/main/java/com/example/zhaodong/dronephotography/activity/ImageAdapter.java
+++ mobile/src/main/java/com/example/zhaodong/dronephotography/activity/ImageAdapter.java
@@ -70,8 +70,6 @@ public class ImageAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageView imageView = new ImageView(mcontext);
-        imageView.setMaxHeight(288);
-        imageView.setMaxWidth(512);
         imageView.setImageBitmap(bitmapList[position]);
         return imageView;
     }
